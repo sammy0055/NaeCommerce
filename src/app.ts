986c7d566ca345d2e99fb9f984a30358a1fc987a
Samuel Ronick <ronicksamuel@gmail.com>
2023-04-28 21:03:23 +0100

    added serverless deployment files

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,27 @@
-import { ApolloServer } from "apollo-server";
-import {
-  startServerAndCreateLambdaHandler,
-  handlers,
-} from '@as-integrations/aws-lambda';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 import "dotenv/config";
 import typeDefs from "./graphql/schema";
 import resolvers from "./graphql/resolvers";
 import connectDB from "./mongoDB";
 import { verifyAccessToken } from "./services/authentication/verify-access-token";
 
-export const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  context: async ({ req }) => ({
-    token: async () => {
-      const token = req.headers.authorization || "";
-      const payload = await verifyAccessToken(token);
-      return { token: payload };
-    },
-  }),
-});
-
-// export const handler = startServerAndCreateLambdaHandler(
-//   server as any,
-//   handlers.createAPIGatewayProxyEventV2RequestHandler(),
-// );
-
+interface MyContext {
+  token?: String;
+}
+export const server = new ApolloServer<MyContext>({ typeDefs, resolvers });
 
 const runServer = async () => {
-  const { url } = await server.listen(4000);
+  const { url } = await startStandaloneServer(server, {
+    context: async ({ req }) => ({
+      token: async () => {
+        const token = req.headers.authorization || "";
+        const payload = await verifyAccessToken(token);
+        return { token: payload };
+      },
+    }),
+    listen: { port: 4000 },
+  });
   console.log(`🚀  Server ready at ${url}`);
 };
 
--- src/handler.ts
+++ src/handler.ts
@@ -1,12 +1,21 @@
-import serverless from "serverless-http";
+import {
+  startServerAndCreateLambdaHandler,
+  handlers,
+} from "@as-integrations/aws-lambda";
 import { server } from "./app";
+import { verifyAccessToken } from "./services/authentication/verify-access-token";
 
-const binaryMimeType = [
-  "application/json",
-  "application/javascript",
-  "application/xml",
-];
-
-export const lambdaHandler = serverless(server as any, {
-  binary: binaryMimeType,
-});
+export const graphqlHandler = startServerAndCreateLambdaHandler(
+  server,
+  // We will be using the Proxy V2 handler
+  handlers.createAPIGatewayProxyEventV2RequestHandler(),
+  {
+    context: async ({ event, context }) => ({
+      token: async () => {
+        const token = event.headers.authorization || "";
+        const payload = await verifyAccessToken(token);
+        return { token: payload };
+      },
+    }),
+  }
+);
