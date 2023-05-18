628ee0402cb37e2185d019eec6b956db94488a3f
Samuel Ronick <ronicksamuel@gmail.com>
2023-05-18 23:14:18 +0100

    added step to sign up

--- api/src/graphql/resolvers/auth/sign-up.ts
+++ api/src/graphql/resolvers/auth/sign-up.ts
@@ -1,6 +1,6 @@
 import { MarchantProfileModel } from "../../../mongoDB/schema";
 import { signUp } from "../../../services/authentication/register";
-import { Result, signUpType } from "../../../types";
+import { RegistrationSteps, Result, signUpType } from "../../../types";
 import { logger } from "../../../utils/logger";
 
 interface signUpTypes {
@@ -11,7 +11,10 @@ export const sign_up = async (_: unknown, { data }: signUpTypes) => {
   try {
     const cognitoUser = await signUp(data.email, data.password);
     const email = cognitoUser.getUsername();
-    await MarchantProfileModel.create({ email: email });
+    await MarchantProfileModel.create({
+      email: email,
+      registrationStep: RegistrationSteps.USERINFO,
+    });
     return Result.Success;
   } catch (error: any) {
     logger(error);
